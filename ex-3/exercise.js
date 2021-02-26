23745b3379f3e2c6d5d377b0d4dd4161b8b816fe
fithawi ghide <lgfetah@gmail.com>
2021-02-26 16:04:49 +0100

    Ex-3 done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,22 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+var arrayOfchoice = ["Black","Barack obama","Paris"];
+
+//window.alert("My first choice is" +" "+ arrayOfchoice[0]);
+
+//console.log("My first choice is" +" "+ arrayOfchoice[0]);
+
+
+var arrayOfchoice ={
+        first: "Black",
+        second: "Barack Obama",
+        thrid: "Paris"
+}
+
+window.alert(arrayOfchoice.first);
+window.alert(arrayOfchoice.second);
+window.alert(arrayOfchoice.thrid);
+
+
