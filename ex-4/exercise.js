ae2d461dc9612280a297f31032226555aa59a06d
fithawi ghide <lgfetah@gmail.com>
2021-03-01 08:32:01 +0100

    Ex-4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,3 +1,17 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function calculate_age(dob) { 
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms); 
+  
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+ 
+}
+
+
+
+window.alert(calculate_age(new Date(1992,4,17)));
+//console.log(calculate_age(new Date(1962, 1, 1)));
+//console.log(calculate_age(new Date(1992,4,17)));
+
