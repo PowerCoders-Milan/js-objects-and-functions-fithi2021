ddc99b16ebad522ff80a7c8c21bc4b40195e61a2
fithawi ghide <lgfetah@gmail.com>
2021-03-01 07:57:50 +0100

    Extra-1 done

---
+++ extra-1/exercise.js
@@ -0,0 +1,20 @@
+
+
+
+function calculate_age(dob) { 
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms); 
+  
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+ 
+}
+
+
+
+window.alert(calculate_age(new Date(1992,4,17)));
+//console.log(calculate_age(new Date(1962, 1, 1)));
+//console.log(calculate_age(new Date(1992,4,17)));
+
+
+
+
